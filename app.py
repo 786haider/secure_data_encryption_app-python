842cbe2d2a05f638cda4827c89df9bf722ce8c95
786haider <haiderhussain536@gmail.com>
2025-04-14 01:45:13 +0500

    commit

--- app.py
+++ app.py
@@ -57,7 +57,6 @@ def decrypt_data(encrypted_text, passkey):
 
 # Streamlit UI
 st.title("🔒 Secure Data Encryption System")
-
 # Navigation
 menu = ["Home", "Store Data", "Retrieve Data", "Login"]
 choice = st.sidebar.selectbox("Navigation", menu, index=menu.index(st.session_state.page) if st.session_state.page in menu else 0)
